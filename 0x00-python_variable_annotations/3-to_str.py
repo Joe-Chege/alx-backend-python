27f3d63cc4b9bc51d12acc882d8a100259d3fc13
Joe-Chege <njorogejosephchege@gmail.com>
2024-01-11 13:30:30 +0000

    to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function that returns the floor of a float."""
+
+
+def to_str(n: float) -> str:
+    """The function takes in n and returns it as float."""
+    return str(n // 1)
