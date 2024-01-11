ee0f3c11424dfb9b627a2670f14b458ed3a22dbd
Joe-Chege <njorogejosephchege@gmail.com>
2024-01-11 13:21:57 +0000

    . Basic annotations - floor

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function that adds two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Adds two strings."""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function that returns the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """The function takes in n and returns it as float."""
+    return int(n // 1)
