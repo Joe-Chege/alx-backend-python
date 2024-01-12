48fe8c7d931666a3f9eb4acae56fee4f739c4d9f
Joe-Chege <njorogejosephchege@gmail.com>
2024-01-12 11:03:26 +0000

    4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
-"""This module contains differnt functions."""
+"""Contains definition of variables with specified values and annotations."""
 
-
-def a(n: float) -> int:
-    """The function takes in n and returns it as int."""
-    return int(n // 1)
-
-
-def pi(n: float) -> float:
-    """The function takes in n and returns it as float."""
-    return float(n // 1)
-
-
-def i_understand_annotations(n: float) -> bool:
-    """The function takes in n and returns it as bool."""
-    return bool(n // 1)
-
-
-def school(n: float) -> str:
-    """The function takes in n and returns it as string."""
-    return str(n // 1)
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
